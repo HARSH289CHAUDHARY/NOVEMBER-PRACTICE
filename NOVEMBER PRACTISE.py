ce43c34085074ec6ea2fa311d589aa309ba3ff0b
HARSH289CHAUDHARY <114799920+HARSH289CHAUDHARY@users.noreply.github.com>
2022-12-14 22:34:31 +0530

    Add files via upload

---
+++ NOVEMBER PRACTISE.py
@@ -0,0 +1,75 @@
+#ordered(Wherever the element is, tuple will fix)
+#they are immutable
+#() is used
+#it allows the duplicate values
+
+
+# mytuple=("CAR","BIKE","TRAIN","BOAT")
+# mylist=list(mytuple)
+# mylist.append("JET")
+# mytuple=tuple(mylist)
+# print(mytuple)
+
+
+# tuple1=(1,2,3,4,5)
+# mylist=list(tuple1)
+# mylist.sort(reverse=True)
+# tuple1=tuple(mylist)
+# print(tuple1)
+
+# tuple=("HARSH CHAUDHARY",)
+# print(tuple)
+
+
+#write a program to unpack the following tuple into four variables and print each variable
+
+# tuple1=(10, 20)
+# tuple2=(30, 40)
+# temp=tuple1
+# tuple1=tuple2
+# temp=tuple2
+# print(tuple1)
+# print(tuple2)
+
+# tuple1 = (1,[6,7],2,3,4,5)
+# a=tuple1[1][0]
+# print(a)
+# tuple1[1][0]=8
+# print(tuple1)
+
+
+
+#Write a program to unpack the following tuple into four variables and print each variable
+
+#tuple1 = (10,20,30,40)
+
+
+# tuple1 = (10, 20)
+# tuple2 = (30, 40)
+#swap above tuples
+#output - tuple1 = (30, 40)
+          #tuple2 = (10,20)
+
+          #tuple1,tuple2 = tuple2,tuple1
+
+#loops
+
+# myTuple = ("car", "boat", "Bike")
+# myTuple.count()
+# for a in myTuple:
+#     print(a)
+# i = 0
+# while i < len(myTuple):
+#     print(myTuple[i])
+#     i += 1
+
+# tuple1 = (1,[6,7],2,3,4,5)
+# a = tuple1[1][0]
+# print(a)
+
+
+# myset = {"car", "bike", "boat"}
+# if "bike" in myset:
+#     print("True")
+# else:
+#     print("False")     
